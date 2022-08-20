7463e84cabf883c9382a08d659fba4f99bd77c42
Harsh <harshgupta3341@gmail.com>
2022-08-20 22:42:13 +0530

    minor change

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
+require(`${__dirname}/db/dbconnect`);
+
+const User = require("./models/user");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(require("./routes/signup"));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");

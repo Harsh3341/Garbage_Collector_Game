db13e5e5b3dd24d0d208bfcac1a69cf761cd33b5
Harsh <harshgupta3341@gmail.com>
2022-08-30 20:19:24 +0530

    Project Reset

--- db/dbconnect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log(err));
-
-module.exports;

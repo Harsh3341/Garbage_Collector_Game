22a3140e728fbabe0d7dae877e7cc9fc3a406b34
Harsh <harshgupta3341@gmail.com>
2022-08-30 22:25:42 +0530

    Register and Login routes and controller added, database connected

---
+++ backend/controllers/registerController.js
@@ -0,0 +1,55 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Missing required fields");
+  }
+
+  // check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(12);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not created");
+  }
+});
+// Generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+module.exports = registerUser;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const colors = require("colors");
 const cors = require("cors");
+const errorHandler = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/register", require("./routes/registerRoute"));
+app.use("/login", require("./routes/loginRoute"));
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

5699a505eac54dd2545955a64ddf51dfbd95dc10
Harsh <harshgupta3341@gmail.com>
2022-08-31 21:30:47 +0530

    done with the backend

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -4,12 +4,18 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const UserGameData = require("../models/userGameData");
 
+// @desc Get user game data
+// @route GET /
+// @access Private
 const getData = asyncHandler(async (req, res) => {
   const gameData = await UserGameData.find({ user: req.user.id });
 
   res.json(gameData);
 });
 
+// @desc Set user game data
+// @route POST /
+// @access Private
 const setData = asyncHandler(async (req, res) => {
   if (!req.body.game || !req.body.score) {
     res.status(400);
